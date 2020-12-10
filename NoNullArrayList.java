1ec194afbe4250e94e83dead702e40204191a4bd
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-12-10 12:14:37 -0500

    Added NoNullArrayList

---
+++ NoNullArrayList.java
@@ -0,0 +1,9 @@
+public class NoNullArrayList<T> extends ArrayList<T> {
+  private ArrayList<T> list;
+
+  public NoNullArrayList() {
+    list = ans;
+    ArrayList<T> ans = new ArrayList<T>(0);
+  }
+  
+}
