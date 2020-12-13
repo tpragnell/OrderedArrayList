646bb2bbc7422cddc38fa95730da0cb21177d789
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-12-13 14:49:34 -0500

    Added tester, completed class

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,7 +6,11 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   private ArrayList<T> list;
 
   public NoNullArrayList() {
-    ArrayList<T> list = new ArrayList<T>(0);
+    list = new ArrayList<T>(0);
+  }
+
+  public NoNullArrayList(int _startingCapacity) {
+    list = new ArrayList<T>(_startingCapacity);
   }
 
   public T set(int _index, T _value) throws IllegalArgumentException{
@@ -14,5 +18,17 @@ public class NoNullArrayList<T> extends ArrayList<T> {
       throw new IllegalArgumentException();
     list.set(_index, _value);
     return _value;
+  }
+
+  public boolean add(T _value) throws IllegalArgumentException{
+    if(_value == null)
+      throw new IllegalArgumentException();
+    return list.add(_value);
+  }
+
+  public void add(int _index, T _value) throws IllegalArgumentException{
+    if(_value == null)
+      throw new IllegalArgumentException();
+    list.add(_index, _value);
   }
 }
