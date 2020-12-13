ea470d13b92154495314975ed4183bee326b5023
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2020-12-13 14:27:58 -0500

    Added NoNullArrayList methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,9 +1,18 @@
+import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
+;
+
 public class NoNullArrayList<T> extends ArrayList<T> {
   private ArrayList<T> list;
 
   public NoNullArrayList() {
-    list = ans;
-    ArrayList<T> ans = new ArrayList<T>(0);
+    ArrayList<T> list = new ArrayList<T>(0);
   }
-  
+
+  public T set(int _index, T _value) throws IllegalArgumentException{
+    if(_value == null)
+      throw new IllegalArgumentException();
+    list.set(_index, _value);
+    return _value;
+  }
 }
